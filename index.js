0d60f20a154aa2e591ffde60c8f4ba8cda7f8060
Mohaimeen Hossain <mhossain5464@gmail.com>
2020-04-05 18:05:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function myAnimal() {
   // You should not need to modify this function
   return animal
 }
-var cat = animal 
+yourAnimal = 'cat' 
+yourAnimal = animal
 function yourAnimal() {
   // The tests expect this function to return `animal` just like the previous function
   // However, you cannot simply modify the existing variable declared on line 1 in the global scope
